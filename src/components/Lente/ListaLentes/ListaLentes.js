533621ebf6928bb065d957befd57e605e98d1e69
jampierzcode <jampierv127@gmail.com>
2023-05-05 23:06:06 -0500

    edit y delete por arreglar

--- src/components/Lente/ListaLentes/ListaLentes.js
+++ src/components/Lente/ListaLentes/ListaLentes.js
@@ -1,16 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { Button, Form, Input, Layout, Select, Table, Tag } from "antd";
-
+import {
+  Form,
+  Input,
+  Layout,
+  Select,
+  Table,
+  Tag,
+  Modal,
+  message,
+  Image,
+  Popconfirm,
+} from "antd";
 // amplify API
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import { API, graphqlOperation } from "aws-amplify";
+import * as mutations from "../../../graphql/mutations";
 import * as queries from "../../../graphql/queries";
 // import { useNavigate } from "react-router-dom";
 
 const { Content } = Layout;
 const { Option } = Select;
 function ListaLentes() {
-  const [editingRow, setEditingRow] = useState(null);
-  const [form] = Form.useForm();
+  // use state de form modal
+  const [grupo, setGrupo] = useState("");
+  const [id, setId] = useState("");
+  const [proveedor, setProveedor] = useState("");
+  const [costo, setCosto] = useState(0);
+  const [precioVenta, setPrecioVenta] = useState(0);
+  const [tiempoEntrega, setTiempoEntrega] = useState("");
+  const [color, setColor] = useState("");
+  const [tipoArmazon, setTipoArmazon] = useState("");
+  const [imagen, setImagen] = useState("");
+  const [tipoMaterial, setTipoMaterial] = useState("");
+
+  const [isEditing, setIsEditing] = useState(false);
+
   // const navigate = useNavigate();
   const [lentes, setLentes] = useState([]);
   const searchGrupos = () => {
@@ -28,29 +52,17 @@ function ListaLentes() {
       key: "grupo",
       dataIndex: "grupo",
       render: (text, record) => {
-        if (editingRow === record.id) {
-          return (
-            <Form.Item name="grupo" valuePropName={record.grupo}>
-              <Select placeholder="Select un grupo DAMA/CABALERO/BOY">
-                <Option value="DAMA">DAMA</Option>
-                <Option value="CABALLERO">CABALLERO</Option>
-                <Option value="BOY">BOY</Option>
-              </Select>
-            </Form.Item>
-          );
-        } else {
-          let color = "";
-          if (text === "DAMA") {
-            color = "geekblue";
-          }
-          if (text === "CABALLERO") {
-            color = "green";
-          }
-          if (text === "BOY") {
-            color = "volcano";
-          }
-          return <Tag color={color}>{text}</Tag>;
+        let color = "";
+        if (text === "DAMA") {
+          color = "geekblue";
         }
+        if (text === "CABALLERO") {
+          color = "green";
+        }
+        if (text === "BOY") {
+          color = "volcano";
+        }
+        return <Tag color={color}>{text}</Tag>;
       },
       filters: searchGrupos(),
       filterMode: "tree",
@@ -61,17 +73,6 @@ function ListaLentes() {
       title: "Proveedor",
       dataIndex: "proveedor",
       key: "proveedor",
-      render: (text, record) => {
-        if (editingRow === record.id) {
-          return (
-            <Form.Item name="proveedor">
-              <Input />
-            </Form.Item>
-          );
-        } else {
-          return <p>{text}</p>;
-        }
-      },
     },
     {
       title: "Costo",
@@ -108,13 +109,14 @@ function ListaLentes() {
         // console.log(key);
         // const image1 = await Storage.get(key, { level: "public" });
         return (
-          <img
+          <Image
             style={{
               width: "80px",
               height: "80px",
               borderRadius: "50%",
               objectFit: "contain",
               objectPosition: "center",
+              overflow: "hidden",
             }}
             src={imagen}
             alt="iamgen prueba"
@@ -134,21 +136,24 @@ function ListaLentes() {
       render: (_, record) => {
         return (
           <>
-            <Button
+            <EditOutlined
               type="link"
               onClick={() => {
-                setEditingRow(record.id);
-                console.log(record);
-                form.setFieldValue({
-                  grupo: record.grupo,
-                  proveedor: record.proveedor,
-                });
-                console.log(form);
+                edithandle(record);
               }}
+            />
+            <Popconfirm
+              title="Eliminar Lente"
+              description="¿Esta seguro de eliminar el lente?"
+              onConfirm={() => deletehandle(record)}
+              okText="Si"
+              cancelText="No"
             >
-              Edit
-            </Button>
-            <Button type="link">Save</Button>
+              <DeleteOutlined
+                onClick={() => changeDelete(record)}
+                style={{ color: "red", marginLeft: "15px" }}
+              />{" "}
+            </Popconfirm>
           </>
         );
       },
@@ -172,6 +177,67 @@ function ListaLentes() {
     );
   };
 
+  const edithandle = (record) => {
+    setId(record?.id);
+    setGrupo(record?.grupo);
+    setProveedor(record?.proveedor);
+    setCosto(record?.costo);
+    setPrecioVenta(record?.precioVenta);
+    setTiempoEntrega(record?.tiempoEntrega);
+    setColor(record?.color);
+    setTipoArmazon(record?.tipoArmazon);
+    setTipoMaterial(record?.tipoMaterial);
+    setIsEditing(true);
+    setImagen(record?.imagen);
+  };
+  const changeDelete = (record) => {
+    setId(record?.id);
+  };
+  const deletehandle = async (record) => {
+    const lente = lentes[record?.id];
+    console.log(lente);
+    try {
+      const lente = {
+        id,
+      };
+      const result = await API.graphql(
+        graphqlOperation(mutations.deleteLENTE, { input: lente })
+      );
+      console.log(result);
+      message.success("El lente se ha eliminado correctamente");
+    } catch (error) {
+      console.log(error);
+      message.error("Hubo un error contacta al administrador");
+    }
+  };
+  const onFinish = async () => {
+    try {
+      const updateLentes = {
+        id,
+        grupo,
+        proveedor,
+        costo,
+        precioVenta,
+        tiempoEntrega,
+        color,
+        tipoArmazon,
+        imagen,
+        tipoMaterial,
+      };
+      console.log(updateLentes);
+      const result = await API.graphql(
+        graphqlOperation(mutations.updateLENTE, { input: updateLentes })
+      );
+      console.log(result);
+      message.success("El lente se ha actualizado");
+      fetchLentes();
+      setIsEditing(false);
+    } catch (error) {
+      console.log(error);
+      message.error("Hubo un error contacta al administrador");
+    }
+  };
+
   useEffect(() => {
     fetchLentes();
   }, []);
@@ -180,18 +246,152 @@ function ListaLentes() {
     <Content>
       <div>
         <p>NUESTOS LENTES</p>
-        <Form form={form}>
-          <Table
-            scroll={{ x: 400 }}
-            rowKey={(record) => record.id}
-            dataSource={lentes}
-            columns={columns}
-            rowClassName="editable-row"
-            // onRow={(lentes) => ({
-            //   onClick: () => navigate(`lentes/${lentes.id}`),
-            // })}
-          />
-        </Form>
+        <Table
+          scroll={{ x: 400 }}
+          rowKey={(record) => record.id}
+          dataSource={lentes}
+          columns={columns}
+          rowClassName="editable-row"
+          // onRow={(lentes) => ({
+          //   onClick: () => navigate(`lentes/${lentes.id}`),
+          // })}
+        />
+        {/* {isEditing === true ? ( */}
+        <Modal
+          onCancel={() => setIsEditing(false)}
+          // onOk={() => setIsEditing(false)}
+          title="Edit Lente"
+          open={isEditing}
+          onOk={() => onFinish()}
+        >
+          <Form layout="vertical">
+            <div
+              style={{
+                display: "grid",
+                gap: "8px",
+                gridTemplateColumns: "repeat(2, 1fr)",
+              }}
+            >
+              <Form.Item
+                label="Grupo"
+                rules={[{ required: true, message: "Este campo es requerido" }]}
+              >
+                <Select
+                  defaultValue={grupo}
+                  onSelect={(e) => setGrupo(e)}
+                  placeholder="Select un grupo DAMA/CABALERO/BOY"
+                >
+                  <Option value="DAMA">DAMA</Option>
+                  <Option value="CABALLERO">CABALLERO</Option>
+                  <Option value="BOY">BOY</Option>
+                </Select>
+              </Form.Item>
+              <Form.Item
+                label="Proveedor"
+                rules={[{ required: true, message: "Este campo es requerido" }]}
+              >
+                <Input
+                  value={proveedor}
+                  onChange={(e) => setProveedor(e.target.value)}
+                />
+              </Form.Item>
+
+              <Form.Item
+                label="Costo"
+                rules={[{ required: true, message: "Este campo es requerido" }]}
+              >
+                <Input
+                  value={costo}
+                  onChange={(e) => {
+                    if (e.target.value === "") {
+                      setCosto(0);
+                    } else {
+                      setCosto(Number(e.target.value));
+                    }
+                  }}
+                />
+              </Form.Item>
+              <Form.Item
+                label="Precio de Venta"
+                rules={[{ required: true, message: "Este campo es requerido" }]}
+              >
+                <Input
+                  value={precioVenta}
+                  onChange={(e) => {
+                    if (e.target.value === "") {
+                      setPrecioVenta(0);
+                    } else {
+                      setPrecioVenta(Number(e.target.value));
+                    }
+                  }}
+                  style={{ width: "100%" }}
+                  // formatter={(value) =>
+                  //   `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                  // }
+                  // parser={(value) => value.replace(/\$\s?|(,*)/g, "")}
+                  // onChange={onChange}
+                />
+              </Form.Item>
+              <Form.Item
+                label="Tiempo Entrega"
+
+                // rules={[{ required: true, message: "Este campo es requerido" }]}
+              >
+                <Input
+                  value={tiempoEntrega}
+                  onChange={(e) => setTiempoEntrega(e.target.value)}
+                />
+              </Form.Item>
+              <Form.Item
+                label="Color"
+                // rules={[{ required: true, message: "Este campo es requerido" }]}
+              >
+                <Input
+                  value={color}
+                  onChange={(e) => setColor(e.target.value)}
+                />
+              </Form.Item>
+              <Form.Item
+                label="Tipo Armazon"
+
+                // rules={[{ required: true, message: "Este campo es requerido" }]}
+              >
+                <Input
+                  value={tipoArmazon}
+                  onChange={(e) => setTipoArmazon(e.target.value)}
+                />
+              </Form.Item>
+              <Form.Item
+                label="Imagen"
+
+                // rules={[{ required: true, message: "Este campo es requerido" }]}
+              >
+                <Input type="file" accept="jpg" />
+                {/* <Upload
+              // customRequest={(info) => handleImage(info)}
+              onChange={(e) => handleImage(e)}
+              action="http://localhost:3000/superadmin"
+              name="lente"
+              accept=".apng,.avif,.gif,.jpg,.jpeg,.jfif,.pjpeg,.pjp,.png,.svg,.webp"
+              listType="picture"
+            >
+              <Button icon={<UploadOutlined />}>Click to upload</Button>
+            </Upload> */}
+              </Form.Item>
+              <Form.Item
+                label="Tipo Material"
+
+                // rules={[{ required: true, message: "Este campo es requerido" }]}
+              >
+                <Input
+                  value={tipoMaterial}
+                  onChange={(e) => setTipoMaterial(e.target.value)}
+                />
+              </Form.Item>
+            </div>
+          </Form>
+        </Modal>
+        {/* ) : null} */}
       </div>
     </Content>
   );
