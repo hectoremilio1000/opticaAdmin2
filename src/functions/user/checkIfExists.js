16851949c0c71eea277ceb4bb05e91acabc1bc45
jampierzcode <jampierv127@gmail.com>
2023-05-11 11:36:26 -0500

    finalizado crear gerentes PTCDMN-8 v2

--- src/functions/user/checkIfExists.js
+++ src/functions/user/checkIfExists.js
@@ -3,23 +3,22 @@ import { Auth } from "aws-amplify";
 export const checkIfExists = async (userName) => {
   let exist = false;
   try {
-    await Auth.signIn(userName, "").then((res) => {
+    await Auth.signIn(userName, "123").then((res) => {
       return false;
     });
   } catch (error) {
     const code = error.code;
-    return error;
-    // switch (code) {
-    //   case "UserNotFoundException":
-    //     exist = false;
-    //     break;
-    //   case "NotAuthorizedException":
-    //   case "PasswordResetRequiredException":
-    //   case "UserNotConfirmedException":
-    //   default:
-    //     exist = true;
-    //     break;
-    // }
-    // return exist;
+    switch (code) {
+      case "UserNotFoundException":
+        exist = false;
+        break;
+      case "NotAuthorizedException":
+      case "PasswordResetRequiredException":
+      case "UserNotConfirmedException":
+      default:
+        exist = true;
+        break;
+    }
+    return exist;
   }
 };
