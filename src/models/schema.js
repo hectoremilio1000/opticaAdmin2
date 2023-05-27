3b805b5632ac88c832571b67a9ef5a91759d155d
hectoremilio1000 <hectoremilio1000@gmail.com>
2023-05-27 14:02:25 -0600

    prueba

--- src/models/schema.js
+++ src/models/schema.js
@@ -1005,6 +1005,6 @@ export const schema = {
         }
     },
     "nonModels": {},
-    "codegenVersion": "3.4.2",
+    "codegenVersion": "3.4.3",
     "version": "cdef99b61b3b9e48faaeaf12e086d896"
 };
